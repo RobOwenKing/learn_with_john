b7c0917f4592e1fa8db7e5895969875d2ed1d1c4
Robert King <robert.o.king@hotmail.co.uk>
2021-12-29 22:07:55 +0000

    chore: Migration run to create new Students table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_29_173528) do
+ActiveRecord::Schema.define(version: 2021_12_29_215720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "students", force: :cascade do |t|
+    t.string "name_zh"
+    t.string "name_en"
+    t.text "onboarding"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
