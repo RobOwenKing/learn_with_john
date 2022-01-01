1d2eec42a0883b594ac6269159c14be591e2d371
Robert King <robert.o.king@hotmail.co.uk>
2022-01-01 17:31:32 +0000

    feat: Draft Practiseds Controller

--- app/controllers/practiseds_controller.rb
+++ app/controllers/practiseds_controller.rb
@@ -1,7 +1,23 @@
 class PractisedsController < ApplicationController
   def create
+    @practised = Practised.new(practiced_params)
+    @student = Student.find(params[:student_id])
+    @practised.save
+
+    redirect_to student_path(@student)
   end
 
   def destroy
+    @practised = Practised.find(params[:id])
+    @student = Student.find(params[:student_id])
+    @practised.destroy
+
+    redirect_to student_path(@student)
+  end
+
+  private
+
+  def practiced_params
+    params.permit(:user_id, :student_id, :topic_id)
   end
 end
