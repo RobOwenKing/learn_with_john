b9383a7267020a575cc3a89f739d9e50d5d275b9
Robert King <robert.o.king@hotmail.co.uk>
2022-01-05 11:50:57 +0000

    feat: Use devise_parameter_sanitizer to allow for setting of User timezone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u[:timezone] = ActiveSupport::TimeZone::MAPPING[u[:timezone]]
+      u.permit(:name, :email, :password, :timezone)
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u[:timezone] = ActiveSupport::TimeZone::MAPPING[u[:timezone]]
+      u.permit(:name, :email, :password, :current_password, :timezone)
+    end
+  end
 end
