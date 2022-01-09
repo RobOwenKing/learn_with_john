f9d593fa1ea820e4daca20e0e194cfa70cbec681
Robert King <robert.o.king@hotmail.co.uk>
2022-01-09 23:40:52 +0000

    feat: Display name of Teacher a Student Practised a Topic with, via find_practised helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def find_practised(practiseds, topic)
+    practiseds[practiseds.find_index { |p| p.topic_id == topic.id }]
+  end
 end
--- app/models/practised.rb
+++ app/models/practised.rb
@@ -1,7 +1,9 @@
 class Practised < ApplicationRecord
   belongs_to :student
   belongs_to :topic
+  belongs_to :user
 
   validates :student, presence: true
   validates :topic, presence: true
+  validates :user, presence: true
 end
