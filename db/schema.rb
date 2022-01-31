220566ba0636217f52389fe5efc2569d48e720b0
Robert King <robert.o.king@hotmail.co.uk>
2022-01-31 21:42:06 +0000

    feat: Create and migrate Categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_30_234224) do
+ActiveRecord::Schema.define(version: 2022_01_31_213945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.boolean "current"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "lessons", force: :cascade do |t|
     t.bigint "user_id"
