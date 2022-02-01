6dabf1e394bd2e75f37a988b8a482b26c54f9237
Robert King <robert.o.king@hotmail.co.uk>
2022-02-01 16:22:48 +0000

    refactor: Routes for Categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-  get 'categories/show'
-  get 'categories/new'
-  get 'categories/create'
-  get 'categories/edit'
-  get 'categories/update'
   devise_for :users, :path_prefix => 'd'
   resources :users, only: [ :index, :show, :edit, :update ]
+
   resources :students do
     resources :practiseds, only: [ :create ]
   end
   resources :practiseds, only: [ :destroy ]
+
+  resources :categories
   resources :topics, only: [:index, :new, :create, :edit, :update, :destroy]
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
